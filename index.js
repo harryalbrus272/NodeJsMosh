be726c2429415ae569ed5dae2e99b5990188df54
shashwatksingh <shashwat@auzmor.com>
2021-11-16 10:05:24 +0530

    environment varibale and adding packages

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+//Major.Minor.Patch
 const _ = require("underscore");
 // Core Modules
 // File or Folder
 // node_modules
 console.log(_.contains([1, 2, 3], 3));
+//
+
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT;
+console.log(PORT);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3]);
+});
+
+// app.post();
+// app.put();
+// app.delete();
+
+app.listen(PORT, () => {
+  console.log("Listening on port 3050");
+});
