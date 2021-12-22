a116a6157bffcb56328b4c0b4a3c0d7a85fc0ea7
shashwatksingh <shashwat@auzmor.com>
2021-12-22 11:08:59 +0530

    adding mongoose and joi schema to the models

--- models/index.js
+++ models/index.js
@@ -55,6 +55,69 @@ function validateMovies (movie) {
   });
   return schema.validate(movie);
 }
+ 
+const Rental = mongoose.model('Rental', new mongoose.Schema({
+  customer: {
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+      },
+      isGold: {
+        type: Boolean,
+        default: false
+      },
+      phone: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+      }      
+    }),  
+    required: true
+  },
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 5,
+        maxlength: 255
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255
+      }  
+    }),
+    required: true
+  },
+  dateOut: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  dateReturned: {
+    type: Date
+  },
+  rentalFee: {
+    type: Number,
+    min: 0
+  }
+}));
+ 
+function validateRental(rental) {
+  const schema = {
+    customerId: Joi.string().required(),
+    movieId: Joi.string().required()
+  };
+ 
+  return Joi.validate(rental, schema);
+}
 
 const Customer = mongoose.model(
   'Customers',
@@ -80,4 +143,4 @@ const Customer = mongoose.model(
   })
 );
 
-module.exports = { Genre, Customer, Movie, genreSchema,  validateGenre, validateMovies};
+module.exports = { Genre, Customer, Movie, Rental, genreSchema,  validateGenre, validateMovies, validateRental};
