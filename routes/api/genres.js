3ff23778b43e392e361b28a0df6869ea8e225c22
shashwatksingh <shashwat@auzmor.com>
2021-12-24 15:51:07 +0530

    adding role based check on deleting genres

--- routes/api/genres.js
+++ routes/api/genres.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Joi = require('joi');
 const auth = require('../../middleware/auth');
+const checkAdmin = require('../../middleware/admin');
 const { Genre } = require('../../models/index');
 
 const schema = Joi.object({
@@ -53,7 +54,7 @@ router.put('/:id', auth, async (req, res) => {
   }
 });
 
-router.delete('/:id', auth, async (req, res) => {
+router.delete('/:id', auth, checkAdmin, async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
   if (!genre) return res.status(404).send('The genre not found');
   res.status(200).send(genre);
