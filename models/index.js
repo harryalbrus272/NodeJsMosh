1c25324a26d0e4209567d2126a34f91fba421e8e
shashwatksingh <shashwat@auzmor.com>
2021-12-24 15:43:23 +0530

    admin property added to the jwt

--- models/index.js
+++ models/index.js
@@ -172,7 +172,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id }, process.env.JWTKEY);
+  const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, process.env.JWTKEY);
   return token;
 };
 
