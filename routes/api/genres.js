239c99216659971ed3ff7696696921481dc89ce6
shashwatksingh <shashwat@auzmor.com>
2021-12-25 09:07:25 +0530

    adding common async middleware and implementing in genres route - solving repetitive trycatch block

--- routes/api/genres.js
+++ routes/api/genres.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Joi = require('joi');
 const auth = require('../../middleware/auth');
 const checkAdmin = require('../../middleware/admin');
+const asyncMiddleware = require('../../middleware/async');
 const { Genre } = require('../../models/index');
 
 const schema = Joi.object({
@@ -16,60 +17,75 @@ const genres = [
   { id: 4, name: 'Biopic' },
 ];
 
-router.get('/', async (req, res, next) => {
-  try {
+router.get(
+  '/',
+  asyncMiddleware(async (req, res) => {
     const genres = await Genre.find({}).sort('name');
     return res.send(genres);
-  } catch (error) {
-    next(error);
-  }
-});
+  })
+);
 
-router.post('/', auth, async (req, res) => {
-  const result = await schema.validate(req.body);
-  const { error } = result;
-  if (!error) {
-    let genres = new Genre({ name: req.body.name });
-    genres = await genres.save();
-    return res.status(200).send(genres);
-  } else {
-    //Bad request
-    const [details] = error.details;
-    console.log(details.message);
-    return res.status(400).send("Genres' name is required");
-  }
-});
+router.post(
+  '/',
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const result = await schema.validate(req.body);
+    const { error } = result;
+    if (!error) {
+      let genres = new Genre({ name: req.body.name });
+      genres = await genres.save();
+      return res.status(200).send(genres);
+    } else {
+      //Bad request
+      const [details] = error.details;
+      console.log(details.message);
+      return res.status(400).send("Genres' name is required");
+    }
+  })
+);
 
-router.put('/:id', auth, async (req, res) => {
-  const result = schema.validate(req.body);
-  if (!result.error) {
-    const genre = await Genre.findByIdAndUpdate(
-      req.params.id,
-      { name: req.body.name },
-      { new: true }
-    );
-    if (!genre) res.status(404).send('The genre not found');
-    return res.status(200).send(genre);
-  } else {
-    //Bad request
-    const [details] = result.error.details;
-    console.log(details.message);
-    return res.status(400).send("Genres'name is required");
-  }
-});
+router.put(
+  '/:id',
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const result = schema.validate(req.body);
+    if (!result.error) {
+      const genre = await Genre.findByIdAndUpdate(
+        req.params.id,
+        { name: req.body.name },
+        { new: true }
+      );
+      if (!genre) res.status(404).send('The genre not found');
+      return res.status(200).send(genre);
+    } else {
+      //Bad request
+      const [details] = result.error.details;
+      console.log(details.message);
+      return res.status(400).send("Genres'name is required");
+    }
+  })
+);
 
-router.delete('/:id', auth, checkAdmin, async (req, res) => {
-  const genre = await Genre.findByIdAndRemove(req.params.id);
-  if (!genre) return res.status(404).send('The genre not found');
-  res.status(200).send(genre);
-});
+router.delete(
+  '/:id',
+  auth,
+  checkAdmin,
+  asyncMiddleware(async (req, res) => {
+    const genre = await Genre.findByIdAndRemove(req.params.id);
+    if (!genre) return res.status(404).send('The genre not found');
+    res.status(200).send(genre);
+  })
+);
 
 //Route parameter
 /** Query string params are also used using req.query  */
-router.get('/:id', async (req, res) => {
-  //res.send(req.params.id, req.params.year);
-  const genre = await Genre.findById(req.params.id);
-  if (!genre) return res.status(404).send('The genre not found');
-  res.status(200).send(genre);
-});
+router.get(
+  '/:id',
+  asyncMiddleware(async (req, res) => {
+    //res.send(req.params.id, req.params.year);
+    const genre = await Genre.findById(req.params.id);
+    if (!genre) return res.status(404).send('The genre not found');
+    res.status(200).send(genre);
+  })
+);
 module.exports = router;
