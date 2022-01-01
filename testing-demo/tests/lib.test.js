01301c1594818878422f52a18815dba56558204a
shashwatksingh <shashwat@auzmor.com>
2022-01-01 20:06:47 +0530

    simple mock interaction testing

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,5 +1,6 @@
 const lib = require('../lib');
 const db = require('../db');
+const mail = require('../mail');
 
 // Grouping a bunch of tests
 describe('Absolute', () => {
@@ -88,5 +89,19 @@ describe('applyDiscount', () => {
     const order = { customerId: 1, totalPrice: 10 };
     const result = lib.applyDiscount(order);
     expect(order.totalPrice).toBe(9);
+  });
+});
+
+describe('notifyCustomer', () => {
+  it('-should sent an email to the customer', () => {
+    db.getCustomerSync = function () {
+      return { email: 'a' };
+    };
+    let mailSent = false;
+    mail.send = function (email, message) {
+      mailSent = true;
+    };
+    lib.notifyCustomer({ customerId: 1 });
+    expect(mailSent).toBe(true);
   });
 });
