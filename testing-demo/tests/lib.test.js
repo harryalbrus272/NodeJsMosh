8bb541ad200ca795a13dc18158a3de365c307855
shashwatksingh <shashwat@auzmor.com>
2022-01-01 16:32:32 +0530

    grouping the tests with describe-it

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,13 +1,17 @@
 const lib = require('../lib');
-test('Absolute -should return a positive number if the input is positive', () => {
+
+// Grouping a bunch of tests
+describe('Absolute', () => {
+  it('-should return a positive number if the input is positive', () => {
     const result = lib.absolute(1);
     expect(result).toBe(1);
-})
-test('Absolute -should return a positive number if the input is positive', () => {
+  });
+  it('-should return a positive number if the input is positive', () => {
     const result = lib.absolute(-1);
     expect(result).toBe(1);
-})
-test('Absolute -should return a 0 if the input is 0', () => {
+  });
+  it('-should return a 0 if the input is 0', () => {
     const result = lib.absolute(0);
     expect(result).toBe(0);
-})
+  });
+});
