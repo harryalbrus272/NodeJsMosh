ca497030b5f3814279285b830c85674dcc545288
shashwatksingh <shashwat@auzmor.com>
2022-01-01 11:42:46 +0530

    resolving the error in the level of winston

--- startup/db.js
+++ startup/db.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 const winston = require('winston');
 module.exports = function () {
   mongoose
-    .connect('mongodb://localhost/vidly')
-    .then(() => winston.log('Connected to MongoDB'));
+    .connect('mongodb://localhost/vidly', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => winston.info('Connected to MongoDB'));
 };
