6e2a585c77b8c4f785f5ba403a68fe74cc084614
shashwatksingh <shashwat@auzmor.com>
2022-01-03 18:40:51 +0530

    adding fake data to db and integration testing

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,16 +1,23 @@
 const request = require('supertest');
+const { Genre, genreSchema } = require('../../models/index');
 let server;
 describe('/api/genres', () => {
   beforeEach(() => {
     server = require('../../index');
   });
-  afterEach(() => {
+  afterEach(async () => {
     server.close();
+    await Genre.deleteMany({});
   });
   describe('GET /', () => {
     it('-should return all genres', async () => {
-        const res = await request(server).get('/api/genres');
-        expect(res.status).toBe(200);
+      await Genre.collection.insertMany([
+        { name: 'genre1' },
+        { name: 'genre2' },
+      ]);
+      const res = await request(server).get('/api/genres');
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBe(2);
     });
   });
 });
