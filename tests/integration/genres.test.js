97253aeb8cac808e46df9292393f8b7fb2896044
shashwatksingh <shashwat@auzmor.com>
2022-01-03 19:37:19 +0530

    resolving errors in the schema of the genres route

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const { Genre, genreSchema } = require('../../models/index');
+const { Genre, User, genreSchema } = require('../../models/index');
 let server;
 describe('/api/genres', () => {
   beforeEach(() => {
@@ -24,18 +24,37 @@ describe('/api/genres', () => {
   });
   describe('GET/:id', () => {
     it('-should return a genre if valid is passed', async () => {
-        const genre = new Genre({name: 'genre1'});
-        await genre.save();
+      const genre = new Genre({ name: 'genre1' });
+      await genre.save();
 
-        const res = await request(server).get('/api/genres/' + genre._id);
+      const res = await request(server).get('/api/genres/' + genre._id);
 
-        expect(res.status).toBe(200);
-        expect(res.body).toHaveProperty('name', genre.name);
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty('name', genre.name);
     });
     it('-should return a 404 if invalid is passed', async () => {
-        const res = await request(server).get('/api/genres/' + 1);
-        
-        expect(res.status).toBe(404);
+      const res = await request(server).get('/api/genres/' + 1);
+
+      expect(res.status).toBe(404);
     });
+  });
+  describe('POST /', () => {
+    it('-should return a 401 if client is not logged in', async () => {
+      const res = await request(server)
+        .post('/api/genres/')
+        .send({ name: 'genre1' });
+
+      expect(res.status).toBe(401);
+    });
+    it('-should return a 400 if client is less than 5 characters', async () => {
+        const token = new User().generateAuthToken();
+        console.log('token', token);
+        const res = await request(server)
+          .post('/api/genres/')
+          .set('x-auth-token', token)
+          .send({ name: 'g' });
+  
+        expect(res.status).toBe(400);
+      });
   });
 });
