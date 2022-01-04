6cd349976945646d61691f3ea0adb83935053d1e
shashwatksingh <shashwat@auzmor.com>
2022-01-04 16:02:40 +0530

    authentication unit testing and moderate changes

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -12,14 +12,14 @@ describe('/api/genres', () => {
   describe('GET /', () => {
     it('-should return all genres', async () => {
       await Genre.collection.insertMany([
-        { name: 'genre1' },
-        { name: 'genre2' },
+        { name: 'genre11' },
+        { name: 'genre22' },
       ]);
       const res = await request(server).get('/api/genres');
       expect(res.status).toBe(200);
       expect(res.body.length).toBe(2);
-      expect(res.body.some((g) => g.name === 'genre1')).toBeTruthy();
-      expect(res.body.some((g) => g.name === 'genre2')).toBeTruthy();
+      expect(res.body.some((g) => g.name === 'genre11')).toBeTruthy();
+      expect(res.body.some((g) => g.name === 'genre22')).toBeTruthy();
     });
   });
   describe('GET/:id', () => {
