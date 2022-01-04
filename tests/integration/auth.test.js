b095c707fd267c2837d0d95e5178b5ebc9a9d228
shashwatksingh <shashwat@auzmor.com>
2022-01-04 15:22:30 +0530

    adding integrations tests in authorization middleware

---
+++ tests/integration/auth.test.js
@@ -0,0 +1,40 @@
+const request = require('supertest');
+const { User, Genre } = require('../../models/index');
+describe('auth middleware', () => {
+  let token;
+  let server;
+  beforeEach(() => {
+    server = require('../../index');
+  });
+
+  afterEach(async () => {
+    server.close();
+    await Genre.deleteMany({});
+  });
+
+  const exec = () => {
+    return request(server)
+      .post('/api/genres/')
+      .set('x-auth-token', token)
+      .send({ name: 'genre1' });
+  };
+
+  beforeEach(() => {
+    token = new User().generateAuthToken();
+  });
+
+  it('-should return 401 if no token is provided', async () => {
+    token = '';
+    const res = await exec();
+    expect(res.status).toBe(401);
+  });
+  it('-should return 400 if invalid token is provided', async () => {
+    token = null;
+    const res = await exec();
+    expect(res.status).toBe(400);
+  });
+  it('-should return 200 if valid token is provided', async () => {
+    const res = await exec();
+    expect(res.status).toBe(200);
+  });
+});
